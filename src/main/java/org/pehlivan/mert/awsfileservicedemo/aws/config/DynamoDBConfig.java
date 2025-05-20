a96974712dc36ecb2a2680771ad0881918a654fd
Mert Pehlivan <pehlivannmert@gmail.com>
2025-05-20 20:37:34 +0300

    add AWS service configuration for DynamoDB, Redis, S3, and SNS

---
+++ src/main/java/org/pehlivan/mert/awsfileservicedemo/aws/config/DynamoDBConfig.java
@@ -0,0 +1,44 @@
+package org.pehlivan.mert.awsfileservicedemo.aws.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.net.URI;
+
+@Configuration
+public class DynamoDBConfig {
+
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${spring.cloud.aws.dynamodb.endpoint}")
+    private String endpointUri;
+
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        return DynamoDbClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .endpointOverride(URI.create(endpointUri))
+                .build();
+    }
+
+    @Bean
+    public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
+        return DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(dynamoDbClient)
+                .build();
+    }
+}
